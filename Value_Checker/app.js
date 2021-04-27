81152c67b2c0e520c2b733ebed873d355ca1c8f7
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-04-27 12:42:44 -0400

    rewritten using arrow fn

--- Value_Checker/app.js
+++ Value_Checker/app.js
@@ -1,14 +1,22 @@
-function valueChecker(arg1, arg2){
-    if(arg1 === arg2){
-        console.log('True')
-    } else {
-        console.log('false')
-    }
-}
+// function valueChecker(arg1, arg2){
+//     if(arg1 === arg2){
+//         console.log('True')
+//     } else {
+//         console.log('false')
+//     }
+// }
 
-let result = valueChecker(2, 2);
-let result2 = valueChecker(4, 'hello');
-let result3 = valueChecker('reskillamericans', 'reskillamericans');
-let result4 = valueChecker(10, 10);
-let result5 = valueChecker(8, '8');
-let result6 = valueChecker('able', 7);
+// let result = valueChecker(2, 2);
+// let result2 = valueChecker(4, 'hello');
+// let result3 = valueChecker('reskillamericans', 'reskillamericans');
+// let result4 = valueChecker(10, 10);
+// let result5 = valueChecker(8, '8');
+// let result6 = valueChecker('able', 7);
+
+let valueChecker = (arg1, arg2) => arg1 === arg2;
+    console.log(valueChecker(2, 2));
+    console.log(valueChecker(4, 'hello'));
+    console.log(valueChecker('reskillamericans', 'reskillamericans'));
+    console.log(valueChecker(10, 10));
+    console.log(valueChecker(8, '8'));
+    console.log(valueChecker('able', 7));
